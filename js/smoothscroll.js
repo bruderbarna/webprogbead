3fe38e4c0294fc61b5db781b4e3069f48669aedb
Bruder Barna <bruder_barna@hotmail.com>
2016-10-31 15:28:19 +0100

    smoothscroll fixed

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -4,6 +4,6 @@
 (function () {
   $('a[href*=#]').on('click', function(event){
       event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+      $('html,body').animate({scrollTop:$(this.hash).offset().top - 52}, 500);
   });
 })();
