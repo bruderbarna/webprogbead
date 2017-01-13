c9704f6e2d34150829dc0f0e4895bae812978e2c
Bruder Barna <bruder_barna@hotmail.com>
2017-01-13 14:11:25 +0100

    progress with gallery and image paddings

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {
+  var isGalleryOpen = false;
+
   $('.gallery .gallery-image').click(function () {
+    console.log("gcl");
     if ($('#lightbox').length > 0) {
       $('#content').html('<img src ="' + $(this).attr("src") + '">');
       $('#lightbox').show('fast');
       $('#navigation').hide();
-    }
-    else {
+    } else {
       $('body').append(
         '<div id="lightbox">' +
           //'<p>Click to close</p>' +
           '<div id="content">' +
               '<img src="#" />' +
           '</div>' +
+          '<h3 class="close-button">click to close</h3>' +
         '</div>'
       );
       $('#content').html('<img src ="' + $(this).attr("src") + '">');
       $('#lightbox').show('fast');
       $('#navigation').hide();
+      isGalleryOpen = true;
     }
   });
 
-  $('#lightbox').on('click', function () {
-    $('#lightbox').hide();
-    $('#navigation').show();
+  $('html').click(function () {
+    if (isGalleryOpen) {
+      $('#lightbox').hide('fast');
+      $('#navigation').show();
+      isGalleryOpen = false;
+    }
   });
 });
